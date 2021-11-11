9a2d82ee2b1aba9c3fb6fd2f4dd138441f55fda6
pandresvaldivia <pandresvaldiviacuzcano@gmail.com>
2021-11-10 22:24:25 -0500

    Brands section created

--- js/functions.js
+++ js/functions.js
@@ -1,13 +1,13 @@
-import { $burgerBtn, $menuContainer, $body } from './selectors.js';
+import { $burgerBtn, $menu, $body } from './selectors.js';
 
 function menuHandle() {
 	const isPressed = $burgerBtn.classList.toggle('is-pressed');
 
 	if (isPressed) {
-		$menuContainer.classList.add('is-visible');
+		$menu.classList.add('is-visible');
 		$body.style.overflowY = 'hidden';
 	} else {
-		$menuContainer.classList.remove('is-visible');
+		$menu.classList.remove('is-visible');
 		$body.style.overflowY = 'auto';
 	}
 }
--- js/selectors.js
+++ js/selectors.js
@@ -1,3 +1,3 @@
 export const $burgerBtn = document.querySelector('.burgerButton');
-export const $menuContainer = document.querySelector('.menu');
+export const $menu = document.querySelector('.menu');
 export const $body = document.querySelector('.body');
