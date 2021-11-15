8a81a38e803c03abc6576cad0bff0453bfb9c827
pandresvaldivia <pandresvaldiviacuzcano@gmail.com>
2021-11-15 17:50:17 -0500

    Animations added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { $burgerBtn } from './selectors.js';
+import { $burgerBtn, $brandSection, $range } from './selectors.js';
 import { menuHandle, resetMenu } from './functions.js';
 
 $burgerBtn.addEventListener('click', menuHandle);
@@ -6,3 +6,17 @@ $burgerBtn.addEventListener('click', menuHandle);
 window.addEventListener('resize', () => {
 	window.innerWidth >= 768 && resetMenu();
 });
+
+const myObserver = new IntersectionObserver(
+	(entries) => {
+		if (entries[0].isIntersecting) {
+			entries[0].target.classList.add('is-visible');
+		}
+	},
+	{
+		threshold: 0.75,
+	}
+);
+
+myObserver.observe($brandSection);
+myObserver.observe($range);
--- js/selectors.js
+++ js/selectors.js
@@ -1,3 +1,5 @@
 export const $burgerBtn = document.querySelector('.burgerButton');
 export const $menu = document.querySelector('.menu');
 export const $body = document.querySelector('.body');
+export const $brandSection = document.querySelector('.brandsSection-content');
+export const $range = document.querySelector('.range');
