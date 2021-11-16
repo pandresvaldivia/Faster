bc2efc360188c93f633f235ef4f5160e052bfc40
pandresvaldivia <pandresvaldiviacuzcano@gmail.com>
2021-11-16 18:11:28 -0500

    HTML, CSS and JS files added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
 import { $burgerBtn, $brandSection, $range, $phones } from './js/selectors.js';
 import { menuHandle, resetMenu, myObserver } from './js/functions.js';
 
+// Styles
+import './css/main.css';
+import './css/icon.css';
+import './css/header.css';
+import './css/burger-button.css';
+import './css/menu.css';
+import './css/hero.css';
+import './css/phone.css';
+import './css/grid-container.css';
+import './css/wrapper.css';
+import './css/button.css';
+import './css/link.css';
+import './css/brands-section.css';
+import './css/checkout.css';
+import './css/purchase.css';
+import './css/track.css';
+import './css/range.css';
+import './css/password.css';
+import './css/section.css';
+import './css/testimonial.css';
+import './css/card.css';
+import './css/sign-up.css';
+import './css/footer.css';
+import './css/footer-menu.css';
+import './css/scrollbar.css';
+import './css/animation.css';
+
 $burgerBtn.addEventListener('click', menuHandle);
 
 window.addEventListener('resize', () => {
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'main.js',
+	},
+	resolve: {
+		extensions: ['.js'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.css$/i,
+				use: [miniCssExtractPlugin.loader, 'css-loader'],
+			},
+		],
+	},
+	plugins: [
+		new htmlWebpackPlugin({
+			inject: true,
+			template: './public/index.html',
+			filename: './index.html',
+		}),
+		new miniCssExtractPlugin(),
+	],
+};
